e60af0c2f3ff817f158537baca7abca6f04eaad4
LICF01 <lucascubilla@gmail.com>
2024-02-22 20:55:54 -0300

    updated hero section and cv file

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,9 +7,9 @@ export const defaultLang = "en";
 
 export const ui = {
   en: {
-    "meta.title": "Lucas Cubilla - Front-End Developer",
+    "meta.title": "Lucas Cubilla - Developer",
     "meta.description":
-      "Lucas Cubilla's dev portfolio showcasing expertise in front-end development, including projects in React, JavaScript, and responsive web design.",
+      "My dev portfolio showcasing my expertise in front-end development with responsive projects using JavaScript, React, NextJS and Astro",
     "meta.author": "Lucas Cubilla",
     "meta.keywords": "Front-End Development, React, JavaScript, Web Design",
     "nav.about": "About",
@@ -19,15 +19,9 @@ export const ui = {
     "hero.name": "I'm Lucas",
     "hero.role": "Front‑End Developer",
     "hero.description":
-      "Building responsive user interfaces with React, Typescript and any css framework",
+      "Over 4 years of experience in frontend development, capable of delivering responsive, accessible, and SEO-optimized projects using JavaScript, React, NextJS, and Astro",
     "about.title": "about",
     "about.description": "A little bit about me",
-    "about.content": [
-      "I am a self-taught developer based in Asunción, Paraguay, and a technology enthusiast. Before programming, I learned to play the cello 🎻 and had the opportunity to play in various orchestras in my country and even on a regular basis in some of them with excellent musicians",
-      "After several years with the cello and taking advantage of the pandemic, I decided to start something new and focused on software development 💻. Since then, I have been programming for approximately 4 years 🕒 and currently, I am focusing on front-end development, using React, Next.js, and Astro 🚀, alongside with CSS frameworks and tools like SASS, TailwindCSS and Styled Components also component libraries like MUI, for testing I've worked with Playwright, Puppeteer and Storybook and want to start with mobile development with React Native. I am also progressing step by step at the university looking to obtain a degree in Computer Engineering 🎓, where so far I have been able to work with languages like C and Java and work remotely in a team with very good colleagues",
-      "Aside from programming, my interests include UI design 🎨, Linux 🐧, keyboards ⌨️ haha, and I enjoy reading, especially science fiction and thriller books. I also usually listen to podcasts, mostly in English, and of course, enjoy listening to music",
-      "In summary, music helped me develop teamwork, constant discipline, the ability to focus on something specific to improve. These skills were fundamental at the beginning of my software development learning process, which reawakened my enthusiasm for learning and improving as a person and a professional",
-    ],
     "experience.title": "Experience & Education",
     "experience.description": "My professional experience and education",
     "projects.title": "Projects",
@@ -44,9 +38,9 @@ export const ui = {
     ],
   },
   es: {
-    "meta.title": "Lucas Cubilla - Desarrollador Front-End",
+    "meta.title": "Lucas Cubilla - Desarrollador",
     "meta.description":
-      "Portafolio de Lucas Cubilla que muestra experiencia en desarrollo front-end, incluyendo proyectos en React, JavaScript, y diseño web responsivo.",
+      "Mi portafolio mostrando mi experiencia en desarrollo front-end, con proyectos responsivos en JavaScript, React, NextJS y Astro.",
     "meta.author": "Lucas Cubilla",
     "meta.keywords": "Desarrollo Front-End, React, JavaScript, Diseño Web",
     "nav.about": "Acerca de",
@@ -56,15 +50,9 @@ export const ui = {
     "hero.name": "Soy Lucas",
     "hero.role": "Desarrollador Front‑End",
     "hero.description":
-      "Construyendo interfaces de usuario responsivas con React, Typescript y cualquier framework de css",
+      "Más de 4 años de experiencia en desarrollo frontend, capaz de entregar proyectos responsivos, accesibles y optimizados para SEO utilizando JavaScript, React, NextJS y Astro",
     "about.title": "Acerca de",
     "about.description": "Para saber un poco más de mi",
-    "about.content": [
-      "Soy un desarrollador radicado en Asunción, Paraguay, autodidacta y entusiasta de la tecnología. Antes de programar, aprendí a tocar el violonchelo 🎻 y tuve la oportunidad de tocar en varias orquestas en mi país, incluso de forma estable en algunas junto a excelentes instrumentistas",
-      "Después de varios años con el violoncello y aprovechando la pandemía, decidí empezar algo nuevo y me enfoqué en el desarrollo de software 💻, desde entonces llevo aproximadamente 4 años programando 🕒 y actualmente me centro en el desarrollo front-end, utilizando React, Next.js y Astro 🚀, junto con frameworks y herramientas de CSS como SASS, TailwindCSS y Styled Components, sumado a experiencia con bibliotecas de componentes como MUI. Para pruebas, he trabajado con Playwright, Puppeteer y Storybook. A parte estoy buscando empezar con el desarrollo de aplicaciones moviles con React Native y avanzando paso a paso en la universidad buscando obtener el título de Ingeniería en Informática 🎓, donde pude tener contacto con lenguajes como C y Java y la experiencia inicial de haber hecho proyectos en equipo remotamente con muy buenos compañeros",
-      "A parte de programar, mis intereses incluyen el diseño de interfaces de usuario 🎨, Linux 🐧, teclados ⌨️ jaja y disfruto de la lectura, especialmente libros  de ciencia ficción y thrillers. También podcasts, normalmente en inglés, y por supuesto, escuchar música",
-      "En resumen, la música me ayudó a desarrollar el trabajo en equipo, una disciplina constante y el ser capaz de enfocarme en algo en específico que mejorar, estas habilidades fueron fundamentales al comienzo de mi proceso de aprendizaje del desarrollo de software, que despertó de nuevo un entusiasmo por aprender y mejorar como persona y profesional",
-    ],
     "experience.title": "Experiencia & Educación",
     "experience.description": "Mi experiencia profesional y educación",
     "projects.title": "Proyectos",
